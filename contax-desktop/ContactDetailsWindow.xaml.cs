b8740cd85b1cbc22c3dc8e488d57387258398a9b
Bally <Bally.Lomibao@howickltd.com>
2024-06-16 10:20:00 +0100

    Initialized Contact details window

---
+++ contax-desktop/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using contax_desktop.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace contax_desktop
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class ContactDetailsWindow : Window
+    {
+        Contact contact;
+        public ContactDetailsWindow(Contact contact)
+        {
+            InitializeComponent();
+
+            this.contact = contact;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (SQLiteConnection connection = new(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Delete(contact);
+            }
+
+            Close();
+
+        }
+    }
+}
